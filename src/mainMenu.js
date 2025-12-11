770d62622757deaaa2864939d8b3f83df4dc2528
Marcus <marcuswilliamj@gmail.com>
2025-12-11 11:52:33 +0100

    Fixed crt flash on menu

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -1,14 +1,15 @@
 const flash = document.getElementById("crt-flash");
-const menu = document.getElementById("main-menu");
+const menu = document.getElementById("main-menu-container");
 
 document.addEventListener("keydown", (event) => {
     if (event.code === "Space") {
         event.preventDefault();
 
         flash.classList.add("active");
+        menu.style.display = "none";
 
         setTimeout(() => {
             window.location.href = "game.html";
-        }, 400);
+        }, 500);
     }
 });
